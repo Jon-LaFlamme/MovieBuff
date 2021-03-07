204482bd31707a79b964abb1c37e79fe7d30fef5
Bryan Settles <Bryansettles19@gmail.com>
2021-03-06 20:30:44 -0500

    Update for filter search

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from flaskr.extensions import db
+from flask_jsglue import JSGlue
 
 csrf = CSRFProtect()
 app = Flask(__name__)
+jsglue = JSGlue(app)
 app.config['SECRET_KEY'] = 'SECRET'
 app.config['WTF_CSRF_ENABLED'] = False
 csrf.init_app(app)
--- flaskr/sqls.py
+++ flaskr/sqls.py
@@ -3,3 +3,27 @@
 title_name = "SELECT * FROM imdblist WHERE title = %s"
 
 title_year_name = 'SELECT imdb_title_id, year, genre from imdblist WHERE Title=%s AND year IN (%s..%s)'
+
+filter_search_rating = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s order by avg_vote desc"
+
+filter_search_date = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s order by year desc"
+
+filter_search_title = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s order by title"
+
+filter_search_rating_language = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s and language in {} order by avg_vote desc"
+
+filter_search_date_language = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s and language in {} order by year desc"
+
+filter_search_title_language = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s and language in {} order by title"
+
+filter_search_rating_genre = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s and genre in {} order by avg_vote desc"
+
+filter_search_date_genre = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s and genre in {} order by year desc"
+
+filter_search_title_genre = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s and genre in {} order by title"
+
+filter_search_rating_language_genre = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s and language in {} and genre in {} order by avg_vote desc"
+
+filter_search_date_language_genre = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s and language in {} and genre in {} order by year desc"
+
+filter_search_title_language_genre = "select title, year, genre, language, avg_vote from imdblist where year >= %s and year <= %s and avg_vote >= %s and avg_vote <= %s and language in {} and genre in {} order by title"
